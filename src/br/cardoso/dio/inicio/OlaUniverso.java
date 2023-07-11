1bfe22d363be6e7dc556165cb049616ad756be2a
Thiago Cardoso <tncardoso@outlook.com>
2023-07-11 12:31:01 -0300

    Iniciando os estudos!

---
+++ src/br/cardoso/dio/inicio/OlaUniverso.java
@@ -0,0 +1,13 @@
+package br.cardoso.dio.inicio;
+
+public class OlaUniverso {
+
+	public static void main(String[] args) {
+		
+		System.out.println(); // Quebrando as linhas...
+		System.out.println("E ai Universo..");
+		System.out.println("Tudo bem por ai???");
+		System.out.println();// Quebrando as linhas...
+
+	}
+}
